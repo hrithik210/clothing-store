cf12af55db7cd17e6b7b72073557f3639d5e53af
hrithik210 <hrithiknayak7829@gmail.com>
2025-03-02 01:10:01 +0530

    added some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <div>
       <Hero />
+      <MainCard />
       <Featured />
       <Collection />
-      <MainCard />
       <Footer />
     </div>
   );
